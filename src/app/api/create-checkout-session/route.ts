f4c1c2a29a6201093757e6b5446c4868f5ed4704
usmannaseem23 <usmannaseem2005@gmail.com>
2025-01-31 22:37:54 +0500

    Added new feature

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -28,8 +28,8 @@ export async function POST(request: Request) {
         quantity: item.quantity,
       })),
       mode: "payment",
-      success_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cart",
+      success_url: "https://ui-ux-hackhathon-e-commerce.vercel.app/success",
+      cancel_url: "https://ui-ux-hackhathon-e-commerce.vercel.app/cart",
     });
 
     return NextResponse.json({ sessionId: session.id });
