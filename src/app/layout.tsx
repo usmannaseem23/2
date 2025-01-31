b37d3f707b7ed6984b5d47d1c2eb941dbcc882aa
usmannaseem23 <usmannaseem2005@gmail.com>
2025-01-31 21:49:24 +0500

    [fourth commit]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,7 @@ export default function RootLayout({
       <html lang="en" className={montserrat.className}>
         <body>
           {children}
-          <script
-            src="//code.tidio.co/x0zp3qbejlod3fyqblv5vphvgsr2ywwh.js"
-            async
-          ></script>
+          
         </body>
       </html>
    </CartProvider>
