2986a64fb19c5f5e3e3624573ee430fdb81b5bcb
usmannaseem23 <usmannaseem2005@gmail.com>
2025-02-07 15:56:54 +0500

    stripe code

--- src/app/(main)/success/page.tsx
+++ src/app/(main)/success/page.tsx
@@ -1,9 +1,21 @@
-import Link from "next/link";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { CheckCircle } from "lucide-react";
+// import Link from "next/link";
 
 export default function PaymentSuccessPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000); // 3 seconds delay before redirecting
+  }, [router]);
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500 wrapper">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500">
       <div className="bg-white rounded-lg shadow-xl p-8 max-w-md w-full">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
           Payment Successful!
@@ -13,9 +25,10 @@ export default function PaymentSuccessPage() {
         </div>
         <p className="text-center text-gray-600 mb-8">
           Thank you for your purchase. Your payment has been processed
-          successfully.
+          successfully. 
         </p>
-        {/* <div className="text-center">
+
+          {/* <div className="text-center">
           <Link
             href="/generate-tracking"
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
@@ -23,6 +36,7 @@ export default function PaymentSuccessPage() {
             Generate Tracking Number!{" "}
           </Link>
         </div> */}
+        
       </div>
     </div>
   );
